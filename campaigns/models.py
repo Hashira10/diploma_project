30af7fd04beb877682597a3f99c31f7a9700216b
tenten <98640569+Hashira10@users.noreply.github.com>
2025-02-12 23:27:16 +0500

    Add files via upload

--- campaigns/models.py
+++ campaigns/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.timezone import now
 
 class Sender(models.Model):
     smtp_host = models.CharField(max_length=255)
@@ -24,3 +25,37 @@ class Recipient(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(Sender, on_delete=models.CASCADE)
+    recipient_group = models.ForeignKey(RecipientGroup, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=255)
+    body = models.TextField()
+    link = models.URLField(blank=True, null=True)
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.sender} to {self.recipient_group}"
+
+
+
+class ClickLog(models.Model):
+    recipient = models.ForeignKey("Recipient", on_delete=models.CASCADE, null=True, blank=True)
+    ip_address = models.GenericIPAddressField()
+    user_agent = models.TextField()
+    timestamp = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return f"Click from {self.ip_address} at {self.timestamp}"
+
+class CredentialLog(models.Model):
+    recipient = models.ForeignKey("Recipient", on_delete=models.CASCADE, null=True, blank=True)
+    email = models.EmailField()
+    password = models.CharField(max_length=255)
+    ip_address = models.GenericIPAddressField()
+    user_agent = models.TextField()
+    timestamp = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return f"Login attempt for {self.email} at {self.timestamp}"
