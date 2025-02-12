600a0045d9094cd94ac1cadf367d496dfb9e3ee8
tenten <98640569+Hashira10@users.noreply.github.com>
2025-02-12 23:32:04 +0500

    Add files via upload

--- phishing_platform/settings.py
+++ phishing_platform/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -64,7 +66,7 @@ ROOT_URLCONF = 'phishing_platform.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -137,3 +139,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+BASE_URL = "http://127.0.0.1:8000"
